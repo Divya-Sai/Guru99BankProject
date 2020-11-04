fd8f9661b6e0743f6fdbe39936d0c93193bc9f25
vaibhav <appautomator@gmail.com>
2020-11-04 14:30:59 +0000

    added code to extent report but it is not working

--- com.guru99.demo/src/main/java/com/guru99/demo/library/ExtentReportsUtil.java
+++ com.guru99.demo/src/main/java/com/guru99/demo/library/ExtentReportsUtil.java
@@ -1,12 +1,20 @@
 package com.guru99.demo.library;
 
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
 public class ExtentReportsUtil {
 
+	ExtentHtmlReporter htmlReporter;
+	com.aventstack.extentreports.ExtentReports extent;
 	ExtentTest test;
-	ExtentReports report;
+	
+	public void getExtentReports(String ReportName) {
+		htmlReporter  = new ExtentHtmlReporter("C:\\Users\\ssaib\\git\\guru99BankProject\\com.guru99.demo\\ExtentReports\\"+ReportName+".html");
+		extent = new com.aventstack.extentreports.ExtentReports();
+		extent.attachReporter(htmlReporter);
+	}
 	
 	
 }
